431002a7f55ae40ca3d3de06c665ac75dc0da8bd
krzshipon <krz.shipon1@gmail.com>
2018-10-14 17:27:51 +0600

    Initial commit

---
+++ app/src/main/java/com/cyclicsoft/calculatorallbase/Calculator_MainActivity.java
@@ -0,0 +1,13 @@
+package com.cyclicsoft.calculatorallbase;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Calculator_MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator__main);
+    }
+}
